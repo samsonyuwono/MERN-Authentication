7d58db30f98e824b6e9437d4ab755b2cbb72f4ed
github username <samson.yuwono@gmail.com>
2018-09-03 19:39:14 -0400

    configured mongoose.js

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ var bodyParser = require("body-parser");
 var book = require("./routes/book");
 var app = express();
 
+var mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
+mongoose
+  .connect(
+    "mongodb://localhost/rec-league-manager-2018",
+    { promiseLibrary: require("bluebird") }
+  )
+  .then(() => console.log("connection succesful"))
+  .catch(err => console.error(err));
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(express.static(path.join(__dirname, "build")));
+
+app.use("/api/book", book);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
